08683785ba95abc151e553c08f5bc75c460f579e
aaronlevin <vilevin@gmail.com>
2022-01-26 15:48:15 -0500

    move albums into new file

--- squalbum.js
+++ squalbum.js
@@ -14,57 +14,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
   const difficulties = [1, 2, 4, 6, 8, 12, 24];
 
-  const albums = [
-    {
-      artist: "Mr. Fingers",
-      title: "Ammnesia",
-      date: new Date(2022, 0, 21),
-      image: "images/2022-01-21.jpg",
-      trivia: "<em>Ammnesia</em> was the first album released by <a href=\"https://en.wikipedia.org/wiki/Larry_Heard\">Larry Heard</a> as Mr. Fingers. The song \"<a href=\"https://www.youtube.com/watch?v=lwObuf_hgfE\">The Juice</a>\" was recently popularized on TikTok."
-    },
-    {
-      artist: "Drake",
-      title: "Views",
-      date: new Date(2022, 0, 22),
-      image: "images/2022-01-22.jpg",
-      trivia: "<a href=\"https://www.youtube.com/watch?v=uxpDa-c-4Mc\">Drake</a> is a Canadian rapper most known for purchasing hundreds of <a href=\"https://en.wikipedia.org/wiki/Aaliyah\">Aaliyah</a> t-shirts that in turn helped fund the infamous Toronto DIY venue Halo Halo. You can't fact check this, but it's true."
-    },
-    {
-      artist: "Beyoncé",
-      title: "Lemonade",
-      date: new Date(2022, 0, 23),
-      image: "images/2022-01-23.jpg",
-      trivia: "<em><a href=\"https://en.wikipedia.org/wiki/Beyonc%C3%A9:_Lemonade\">Lemonade</a></em> is the sixth album from Beyoncé, and her second \"visual\" album. The album received universal critical acclaim, including MTV\'s best music of the year for the song \"<a href=\"https://www.youtube.com/watch?v=WDZJPJV__bQ\">Formation</a>\""
-    },
-    {
-      artist: "Drexciya",
-      title: "Neptune's Lair",
-      date: new Date(2022, 0, 24),
-      image: "images/2022-01-24.jpg",
-      trivia: "<em>Neptune\'s Lair</em> is the second album from Detroit-based techno duo <a href=\"https://en.wikipedia.org/wiki/Drexciya\">Drexciya</a> and their first on the <a href=\"https://en.wikipedia.org/wiki/Tresor_(club)\">Tresor</a> label. While relatively late in their career, tracks like \"<a href=\"https://www.youtube.com/watch?v=e1Kg_BvCuy4\">Andreaen Sand Dunes</a>\" capture them in all their aquatic glory."
-    },
-    {
-      artist: "Boards of Canada",
-      title: "Music Has The Right To Children",
-      date: new Date(2022, 0, 25),
-      image: "images/2022-01-25.jpg",
-      trivia: "Internet people spend their entire lives trying to figure out what synthesizers are used on Boards of Canada's <a href=\"https://www.youtube.com/playlist?list=OLAK5uy_mt1uPu5r3DeEptU-yQ5_DYLS5t_qU0C30\">breakthrough album</a> from 1998."
-    },
-    {
-      artist: "Steely Dan",
-      title: "Katy Lied",
-      date: new Date(2022, 0, 26),
-      image: "images/2022-01-26.jpg",
-      trivia: "Who are these children that scheme and run wild? What are the secrets they trace in the sky? And why do you tremble each time they ride by? Throw out <a href=\"https://www.youtube.com/watch?v=QzAJqKll6hI\">your gold teeth</a> and see how they roll."
-    },
-    {
-      artist: "Kanye West",
-      title: "808s & Heartbreak",
-      date: new Date(2022, 0, 27),
-      image: "images/2022-01-27.jpg",
-      trivia: "To be honest I've never listen to this album on account of its title."
-    }
-  ];
+  const albums = importAlbums();
   const urlParams = new URLSearchParams(window.location.search);
 
   var canvas = document.getElementById('canvas');
